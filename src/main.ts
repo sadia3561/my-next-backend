12a8f4fd35874cd9a5c5880e1128034a81e23349
sadiya Bepari <sadiya3561@gmail.com>
2025-11-27 15:24:25 +0530

    hy

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,19 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+
+  // CORS properly configure
+  app.enableCors({
+    origin: 'https://my-next-frontend-seven.vercel.app', // frontend URL
+    methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true, // cookies/session ke liye
+  });
+
   app.use(json({ limit: '5mb' }));
 
   const port = process.env.PORT ? Number(process.env.PORT) : 4000;
-  await app.listen(port, '0.0.0.0');  // <- IMPORTANT
+  await app.listen(port, '0.0.0.0');
 
   console.log(`🚀 Backend is running on port ${port}`);
 }
