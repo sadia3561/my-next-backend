adf9535b9930ed23ce268173712bfadc9bb7354c
sadiya Bepari <sadiya3561@gmail.com>
2025-12-15 17:36:29 +0530

    sss

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,8 +1,25 @@
-//src/mail/mail.module.ts
+// src/mail/mail.module.ts
 import { Module } from '@nestjs/common';
+import { MailerModule } from '@nestjs-modules/mailer';
 import { MailService } from './mail.service';
 
 @Module({
+  imports: [
+    MailerModule.forRoot({
+      transport: {
+        host: process.env.SMTP_HOST,
+        port: Number(process.env.SMTP_PORT),
+        secure: Number(process.env.SMTP_PORT) === 465, // true for gmail
+        auth: {
+          user: process.env.SMTP_EMAIL,
+          pass: process.env.SMTP_PASSWORD,
+        },
+      },
+      defaults: {
+        from: `"No Reply" <${process.env.SMTP_EMAIL}>`,
+      },
+    }),
+  ],
   providers: [MailService],
   exports: [MailService],
 })
